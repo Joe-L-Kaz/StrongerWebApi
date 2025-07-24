cd37af76317e6fa262538ee9f99eeb035d05b287
Joseph <joseph.lobokazinczi@hotmail.com>
2025-07-24 20:50:04 +0100

    remove redundant code

--- src/Stronger.Domain/Entities/EntityBase.cs
+++ src/Stronger.Domain/Entities/EntityBase.cs
@@ -5,11 +5,5 @@ namespace Stronger.Domain.Entities;
 public abstract class EntityBase<T> where T : struct
 {
     public T Id { get; set; }
-
     public DateTime CreatedAt { get; set; }
-
-    public EntityBase()
-    {
-        CreatedAt = DateTime.UtcNow;
-    }
 }
